0a458e9a9ba843a0ae85cfe037a0fb43613bf779
Colette Ulianogit config --global --listclear <culiano@live.com>
2023-01-13 08:23:02 -0500

    update files

--- flask_app/controllers/authors_controller.py
+++ flask_app/controllers/authors_controller.py
@@ -1,45 +1,45 @@
 from flask_app import app
 from flask import render_template, redirect, request, session
-from flask_app.models.dojo import Dojo
+from flask_app.models import author_model
 
 # ? --------------------------------------
 # Index page redirects to /dojos
 @app.route('/') 
 def index():
-    return redirect("/dojos")  
+    return redirect("/authors")  
 # ? --------------------------------------
 
 
 
 # ? --------------------------------------
-# READ all dojos, display on frontend
-@app.route('/dojos') 
-def all_dojos():
-    return render_template("index.html", dojos = Dojo.get_all())  
+# READ all authors, display on frontend
+@app.route('/authors') 
+def all():
+    return render_template("index.html", authors = author_model.Author.get_all())  
 # ? --------------------------------------
 
 
 
 # ? --------------------------------------
-# CREATE new dojo, POST data
-@app.route('/create/dojo', methods=['POST']) 
-def create_dojo():
-    Dojo.save(request.form)
+# CREATE new author, POST data
+@app.route('/create/author', methods=['POST']) 
+def create():
+    author_model.Author.save(request.form)
 
-    return redirect('/') 
+    return redirect('/authors') 
 # ? --------------------------------------
 
 
 
 # ? --------------------------------------
 # READ one dojo, show on frontend
-@app.route('/view/dojo/<int:id>') 
-def view_dojo(id):
-    data = { 
-        "id": id 
-    }
+# @app.route('/view/dojo/<int:id>') 
+# def view_dojo(id):
+#     data = { 
+#         "id": id 
+#     }
 
-    return render_template("show_dojo.html", dojo = Dojo.get_dojo_with_ninjas(data))  
+#     return render_template("show_dojo.html", dojo = Dojo.get_dojo_with_ninjas(data))  
 # ? --------------------------------------
 
 
@@ -47,21 +47,21 @@ def view_dojo(id):
 # ? --------------------------------------
 # Collectively this is to EDIT a dojo
 # READ one dojos, show on frontend in filled-out form
-@app.route('/update/dojo/<int:id>') 
-def update_user(id):   
-    data = { 
-        "id": id 
-    }
+# @app.route('/update/dojo/<int:id>') 
+# def update_user(id):   
+#     data = { 
+#         "id": id 
+#     }
 
-    return render_template("update_dojo.html", dojo = Dojo.get_one(data))  
+#     return render_template("update_dojo.html", dojo = Dojo.get_one(data))  
 
 # UPDATE dojos, collect form data
 # need hidden input on the form with the dojos id
-@app.route('/update/dojo', methods=['POST']) 
-def update_user_form():
-    Dojo.update_one(request.form)
+# @app.route('/update/dojo', methods=['POST']) 
+# def update_user_form():
+#     Dojo.update_one(request.form)
 
-    return redirect("/")  
+#     return redirect("/")  
 # ? --------------------------------------
 
 
---
+++ flask_app/models/author_model.py
@@ -0,0 +1,102 @@
+from flask_app.config.mysqlconnection import connectToMySQL
+from flask_app.models import book_model
+
+
+
+class Author:
+    def __init__(self, data):
+        self.id = data['id']
+        self.name = data['name']
+        self.created_at = data['created_at']
+        self.updated_at = data['updated_at']
+
+
+
+# ? --------------------------------------
+    # READ all authors, display on frontend
+    @classmethod
+    def get_all(cls):
+        query = "SELECT * FROM authors;"
+        results = connectToMySQL("books_schema").query_db(query)
+
+        authors = []
+
+        for author in results:
+            authors.append(cls(author))
+        
+        return authors
+# ? --------------------------------------
+
+
+
+# ? --------------------------------------
+    # CREATE new dojos, add form data to database
+    @classmethod
+    def save(cls, data):
+        query = "INSERT INTO authors (name, created_at, updated_at) VALUES (%(author)s, NOW(), NOW());"
+
+        return connectToMySQL("books_schema").query_db(query, data)
+# ? --------------------------------------
+
+
+
+# ? --------------------------------------
+    # @classmethod
+    # def get_dojo_with_ninjas(cls, data):
+    #     query = "SELECT * FROM dojos LEFT JOIN ninjas ON dojos.id = ninjas.dojo_id WHERE dojos.id = %(id)s;"
+
+    #     results = connectToMySQL("dojos_and_ninjas_schema").query_db(query, data)
+
+    #     dojo = cls(results[0])
+        # ! what to do if there is no ninja added to a dojo, how to stop None and the action links from appearing on the frontend
+
+        # for row in results:
+        #     data = {
+        #         "id": row['ninjas.id'],
+        #         "first_name": row['first_name'],
+        #         "last_name": row['last_name'],
+        #         "age": row['age'],
+        #         "created_at": row['ninjas.created_at'],
+        #         "updated_at": row['ninjas.updated_at']
+        #     }
+        #     dojo.ninjas.append(ninja.Ninja(data))
+            
+        # print(dojo)
+        # return dojo
+# ? --------------------------------------
+
+
+
+# ? --------------------------------------
+    # READ one dojos, show on frontend
+    # @classmethod
+    # def get_one(cls, data):
+    #     query  = "SELECT * FROM dojos WHERE id = %(id)s;" 
+    #     result = connectToMySQL('dojos_and_ninjas_schema').query_db(query, data)
+
+    #     return cls(result[0]) 
+# ? --------------------------------------
+
+
+
+# ? --------------------------------------
+    # UPDATE dojos with form data
+    # @classmethod
+    # def update_one(cls, data):
+    #     query  = "UPDATE dojos SET name = %(new_dojo)s, updated_at = NOW() WHERE id = %(id)s;" 
+
+    #     return connectToMySQL('dojos_and_ninjas_schema').query_db(query, data)
+# ? --------------------------------------
+
+
+
+# ? --------------------------------------
+    # DELETE dojos
+    # ! How would you handle deleting a dojo if there were ninjas assigned to it?
+    # @classmethod
+    # def remove_one(cls, data):
+    #     query = "DELETE FROM dojos WHERE id = %(id)s;"
+
+    #     return connectToMySQL('dojos_and_ninjas_schema').query_db(query, data)
+# ? --------------------------------------
+
